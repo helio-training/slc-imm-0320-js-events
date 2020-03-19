e266276cd2c854c37165a47a865d9ca9af032bcf
Wes Cutting <wesleycutting@gmail.com>
2020-03-19 10:39:01 -0600

    Added unique behaviour to each button

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ const response = document.getElementById('response');
 
 const btnClicked = (event) => {
     console.log(event.target.id);
+
+    // response.innerHTML = my_input.value;
+
+    switch(event.target.id){
+        case 'family':
+            response.innerHTML = 'Edited for Family Friendly Places';
+            my_input.value = '';
+            break;
+        case 'nsfw':
+            response.innerHTML = 'How dare you?!? *!$% you, *#&@!!!!';
+            my_input.value = '';
+            break;
+        case 'water':
+            response.innerHTML = 'Drinking ' + my_input.value;
+            my_input.value = '';
+            break;
+        case 'tuperware':
+            response.innerHTML = my_input.value + ' is in storage!'
+            my_input.value = '';
+            break;
+    }
 }
 
 family.addEventListener('click', btnClicked);
