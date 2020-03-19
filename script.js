99d43caedfbb832ef5d5bf2c4dcdd7a257862fbc
Wes Cutting <wesleycutting@gmail.com>
2020-03-19 09:50:45 -0600

    Connected HTML to JS

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Welcome to JavaScript Events');
+
+const family = document.getElementById('family');
+const nsfw = document.getElementById('nsfw');
+const water = document.getElementById('water');
+const tuperware = document.getElementById('tuperware');
+const my_input = document.getElementById('my_input');
+const response = document.getElementById('response');
+
