053bacf49eb3fa581c2a66391179cf17e0a72d38
Wes Cutting <wesleycutting@gmail.com>
2020-03-19 13:36:47 -0600

    Implement sharing data between pages

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('Welcome to JavaScript Events');
+// console.log(localStorage.getItem('name'))
 
 const family = document.getElementById('family');
 const nsfw = document.getElementById('nsfw');
@@ -14,25 +15,29 @@ const btnClicked = (event) => {
 
     switch(event.target.id){
         case 'family':
-            response.innerHTML = 'Edited for Family Friendly Places';
+            // response.innerHTML = 'Edited for Family Friendly Places';
+            response.innerHTML = 'Family Stored'
+            localStorage.setItem('input', my_input.value)
             my_input.value = '';
             break;
         case 'nsfw':
             response.innerHTML = 'How dare you?!? *!$% you, *#&@!!!!';
+            localStorage.setItem('name', my_input.value)
             my_input.value = '';
             break;
         case 'water':
-            response.innerHTML = 'Drinking ' + my_input.value;
-            my_input.value = '';
+            // response.innerHTML = 'Drinking ' + my_input.value;
+            // my_input.value = '';
+            response.innerHTML = localStorage.getItem('input')
             break;
         case 'tuperware':
-            response.innerHTML = my_input.value + ' is in storage!'
-            my_input.value = '';
+            response.innerHTML = 'Food is in the fridge!'
             break;
     }
 }
 
+// The first value can be: click, dblclick, mouseover, mouseout
 family.addEventListener('click', btnClicked);
 nsfw.addEventListener('click', btnClicked);
-water.addEventListener('click', btnClicked);
-tuperware.addEventListener('click', btnClicked);
+water.addEventListener('dblclick', btnClicked);
+tuperware.addEventListener('dblclick', btnClicked);
